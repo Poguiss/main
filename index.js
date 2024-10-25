305ab07d1e3e9d3553cea144c0551b6f37ef42f9
Poguiss <166633789+Poguiss@users.noreply.github.com>
2024-10-25 10:02:06 -0300

    teste

--- index.js
+++ index.js
@@ -12,3 +12,30 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+
+
+const modal = document.getElementById("myModal");
+const modalImg = document.getElementById("imgModal");
+const closeBtn = document.getElementsByClassName("close")[0];
+
+
+document.querySelectorAll('.produto img').forEach(img => {
+    img.addEventListener('click', function () {
+        modal.style.display = "block"; 
+        modalImg.src = this.src;     
+    });
+});
+
+
+closeBtn.addEventListener('click', function () {
+    modal.style.display = "none";
+});
+
+
+modal.addEventListener('click', function (event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+});
+
+
